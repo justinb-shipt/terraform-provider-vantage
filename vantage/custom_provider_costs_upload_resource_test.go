84b546d92604c2ea670b461ffadcfcfdda076acc
Justin Bedard <justin.bedard@shipt.com>
2026-04-22 16:17:44 -0400

    Not sure why copilot used single quotes for a string constant.

--- vantage/custom_provider_costs_upload_resource_test.go
+++ vantage/custom_provider_costs_upload_resource_test.go
@@ -69,9 +69,9 @@ func TestAccCustomProviderCostsUploadResource_autoTransform(t *testing.T) {
 	resourceName := "vantage_custom_provider_costs_upload.test"
 
 	// Non-FOCUS CSV that requires auto_transform to be processed by Vantage.
-	const csv = `date,service,category,cost,description
+	const csv = "date,service,category,cost,description
 2024-08-01,vm_server,compute,150.00,onprem_cluster1
-2024-08-01,storage_array,storage,50.00,onprem_nas`
+2024-08-01,storage_array,storage,50.00,onprem_nas"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(t) },
